e0b0adb20309d8be544aa7d267046464340d43f5
ImLostPlsHelp <maurits1005@gmail.com>
2025-08-14 13:01:52 +0700

    Finished the articles, articles.json, and displaying it in grid using css

--- main.js
+++ main.js
@@ -11,3 +11,22 @@ fetch('./categories.json').then(response => {
         document.querySelector('.categories-list').appendChild(categoryElement);
     }
 })
+
+fetch('./articles.json').then(response => {
+    if(response.ok) {
+        return response.json();
+    } else {
+        throw new Error('Something went wrong');
+    }
+}).then(data => {
+    for(const article of data.articles) {
+        const cardElement = document.createElement('div');
+        cardElement.className = 'article-card';
+        cardElement.innerHTML = `
+            <p><strong>${article.title}</strong></h2>
+            <p>${article.description}</p>
+            <p>${article.date} - ${article.category}`
+
+        document.querySelector('.articles').appendChild(cardElement);
+    }
+})
